3a123a6b1e72b5a7ebff9395cfae16c001e76e4b
warren-wupeng <warren.wu@visionwx.com>
2022-10-24 19:30:14 +0800

    all completed

---
+++ step25/createStatementData25.py
@@ -0,0 +1,64 @@
+def createStatementData(invoice, plays):
+    def enrichPerformance(aPerformance):
+
+        def playFor(aPerformance):
+            play = plays[aPerformance['playID']]
+            return play
+
+        def volumeCreditFor(aPerformance):
+            # add volume credits
+            result = 0
+            result += max(aPerformance['audience'] - 30, 0)
+            # add extra credit for every five comedy attendees
+            if "comedy" == aPerformance['play']['type']:
+                result += round(aPerformance['audience'] / 5)
+            return result
+
+        calculator = PerformanceCalculator(aPerformance, playFor(aPerformance))
+        result = dict() | aPerformance
+        result['play'] = calculator.play
+        result['amount'] = calculator.amount
+        result['volumeCredits'] = volumeCreditFor(result)
+        return result
+
+    statementData = dict()
+    statementData['customer'] = invoice['customer']
+    statementData['performances'] = list(
+        map(enrichPerformance, invoice['performances'])
+    )
+    statementData['totalAmount'] = totalAmount(statementData)
+    statementData['totalVolumeCredits'] = totalVolumeCredits(statementData)
+    return statementData
+
+
+class PerformanceCalculator:
+
+    def __init__(self, aPerformance, aPlay):
+        self.performance = aPerformance
+        self.play = aPlay
+
+    @property
+    def amount(self):
+
+        if self.play['type'] == "tragedy":
+            result = 40000
+            if self.performance['audience'] > 30:
+                result += 1000 * (self.performance['audience'] - 30)
+        elif self.play['type'] == "comedy":
+            result = 30000
+            if self.performance['audience'] > 20:
+                result += 10000 + 500 * (self.performance['audience'] - 20)
+                result += 300 * self.performance['audience']
+        else:
+            raise ValueError(f"unknown type: {self.play['type']}")
+        return result
+
+
+def totalAmount(data):
+    result = sum(perf['amount'] for perf in data['performances'])
+    return result
+
+
+def totalVolumeCredits(data):
+    result = sum(perf['volumeCredits'] for perf in data['performances'])
+    return result
---
+++ step28/createStatementData28.py
@@ -0,0 +1,85 @@
+def createStatementData(invoice, plays):
+    def enrichPerformance(aPerformance):
+
+        def playFor(aPerformance):
+            play = plays[aPerformance['playID']]
+            return play
+
+        calculator = PerformanceCalculator.create(
+            aPerformance, playFor(aPerformance)
+        )
+        result = dict() | aPerformance
+        result['play'] = calculator.play
+        result['amount'] = calculator.amount
+        result['volumeCredits'] = calculator.volumeCredit
+        return result
+
+    statementData = dict()
+    statementData['customer'] = invoice['customer']
+    statementData['performances'] = list(
+        map(enrichPerformance, invoice['performances'])
+    )
+    statementData['totalAmount'] = totalAmount(statementData)
+    statementData['totalVolumeCredits'] = totalVolumeCredits(statementData)
+    return statementData
+
+
+class PerformanceCalculator:
+
+    @classmethod
+    def create(cls, aPerformance, aPlay):
+        if aPlay['type'] == 'tragedy':
+            return TragedyCalculator(aPerformance, aPlay)
+        elif aPlay['type'] == 'comedy':
+            return ComedyCalculator(aPerformance, aPlay)
+        else:
+            raise TypeError(f"unknown type: {aPlay['type']}")
+
+    def __init__(self, aPerformance, aPlay):
+        self.performance = aPerformance
+        self.play = aPlay
+
+    @property
+    def amount(self):
+        raise NotImplementedError
+
+    @property
+    def volumeCredit(self):
+        # add volume credits
+        result = 0
+        result += max(self.performance['audience'] - 30, 0)
+        # add extra credit for every five comedy attendees
+        if "comedy" == self.performance['play']['type']:
+            result += round(self.performance['audience'] / 5)
+        return result
+
+
+class TragedyCalculator(PerformanceCalculator):
+
+    @property
+    def amount(self):
+        result = 40000
+        if self.performance['audience'] > 30:
+            result += 1000 * (self.performance['audience'] - 30)
+        return result
+
+
+class ComedyCalculator(PerformanceCalculator):
+
+    @property
+    def amount(self):
+        result = 30000
+        if self.performance['audience'] > 20:
+            result += 10000 + 500 * (self.performance['audience'] - 20)
+            result += 300 * self.performance['audience']
+        return result
+
+
+def totalAmount(data):
+    result = sum(perf['amount'] for perf in data['performances'])
+    return result
+
+
+def totalVolumeCredits(data):
+    result = sum(perf['volumeCredits'] for perf in data['performances'])
+    return result
